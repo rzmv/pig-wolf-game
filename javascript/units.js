68f6843f7351016f536e276d5f17648258f923b0
=Danilov Roman <danilov.roman97@gmail.com>
2016-07-11 22:15:50 +1000

    Add wolf's trajectory class.

--- javascript/units.js
+++ javascript/units.js
@@ -3,7 +3,8 @@ const point = require('./cells').point;
 
 const initialHealth = 3;
 const initialMana = 10;
-const initialSpeed = 1;
+// FIXMYNAME: actually it is 1 / speed, i.e. time needed to go by 1 cell
+const initialSpeed = 1000;
 
 class Unit {
   constructor(position, speed) {
@@ -31,10 +32,53 @@ class Pig extends Unit {
 
 }
 
+class Trajectory {
+  
+  getTrajectoryStep (position) {
+    for (let i = 0; i < this._trajectory.length; ++i)
+      if (this._trajectory[i] == position)
+        return i;
+    
+    // we're not staying on the trajectory
+    // then we'll stay at the same place
+    this._trajectory = [position];
+    this._currentDirection = 0;
+    return 0;
+  }
+  
+  constructor(position, trajectory) {
+    this._trajectory = trajectory;
+    this._currentDirection = 1;
+    this._currentStep = this.getTrajectoryStep(position);    
+  }
+
+  currentPosition() {
+    return this._trajectory[this._currentStep];
+  }
+
+  move() {
+    let curStep = this._currentStep;
+    let curDir = this._currentDirection;
+    
+    if ((curStep + curDir == this._trajectory.length) ||
+       (curStep + curDir < 0))
+    {
+      this._currentDirection = -this._currentDirection;  
+    }
+    this._currentStep += this._currentDirection;
+  }
+}
+
 class Wolf extends Unit {
   constructor(position, speed, trajectory) {
     super(position, speed);
-    this.trajectory = trajectory;
+    this.trajectory = new Trajectory(position, trajectory);
+    this.position = () => this.trajectory.currentPosition;
+    // setInterval(this.move, speed);
+  }
+
+  move() {
+    
   }
 }
 
