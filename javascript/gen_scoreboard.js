29ea16a37b06c0f99af12b0ec8ad2ceab5de3c46
Max <acmxca@gmail.com>
2016-07-14 03:23:45 +1000

    Upgrade scoreboard

--- javascript/gen_scoreboard.js
+++ javascript/gen_scoreboard.js
@@ -3,23 +3,31 @@ window.onload = function() {
   if (db.auth() !== "ok") {
     alert("Error. Please, refresh the page"); //TODO: replace alert with normal notification
   } else {
-    db.getLeaderBoard(10, function(usersScores) {
+    db.getLeaderBoard(25, function(usersScores) {
       for (let i = 0; i < usersScores.length; i++) {
-        addTableRow(usersScores[i].username, usersScores[i].score);
+        console.log(usersScores[i].score);
+        addTableRow(i + 1, usersScores[i].username, usersScores[i].score);
       }
     });
   }
 }
 
-function addTableRow(username, score) {
+function addTableRow(id, username, score) {
   let tableRow = document.createElement('tr');
+
+  let idCell = document.createElement('td');
+  idCell.innerText = id;
 
   let userCell = document.createElement('td');
   userCell.innerText = username;
-  tableRow.appendChild(userCell);
 
   let scoreCell = document.createElement('td');
   scoreCell.innerText = score;
+
+  userCell.className = scoreCell.className = "scorebCell";
+  idCell.className += 'scorebCell numberCell';
+  tableRow.appendChild(idCell);
+  tableRow.appendChild(userCell);
   tableRow.appendChild(scoreCell);
 
   document.getElementById('scoreboard').appendChild(tableRow);
