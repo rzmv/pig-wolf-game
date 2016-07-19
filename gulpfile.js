022cde5a432c1c01202cd59cfd6d1f103ef62579
Max <acmxca@gmail.com>
2016-07-19 11:51:45 +1000

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+// Starts a test server, which you can view at http://localhost:8000
+var gulp      = require('gulp');
+var webserver = require('gulp-webserver');
+
+gulp.task('server', function() {
+  gulp.src('./')
+  .pipe(webserver({
+    port: 8000,
+    host: 'localhost',
+    fallback: 'index.html',
+    livereload: true,
+    open: true
+  }));
+});
